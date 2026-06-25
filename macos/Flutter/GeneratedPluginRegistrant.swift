74deb75b70a409a580f388301c728061668f03b9
Ngô Tiến Tới <ngotientoi21@gmail.com>
2026-06-26 02:58:15 +0700

    chore(register file_selector): Khi thêm plugin file_selector (hoặc bất kỳ plugin nào hỗ trợ desktop, linux, macos), Flutter sẽ tự động cập nhật file này để đăng ký (register) plugin cho nền tảng.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import file_selector_macos
 import firebase_core
 import geolocator_apple
 import mobile_scanner
@@ -16,6 +17,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
